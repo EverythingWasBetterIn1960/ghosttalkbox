f829863a80e3274e358655a942252f193649670b
Brendan Meyer <brenmeyer2245@gmail.com>
2018-09-29 09:04:38 -0500

    Added GET interaction and GET root interaction routes

--- server/api/interaction.js
+++ server/api/interaction.js
@@ -3,7 +3,35 @@ const {Interaction, Character} = require('../db/models');
 
 module.exports = router;
 
-router.use('/:interactionId', async (req,res,next) => {
+/**
+ * Interactions can either be found by root or node.
+ * If root, the request is for the root interaction in a character's conversation.
+ * If node, the request is for a node in the interaction tree. Node requests still utilize direct lookup by interaction id.
+ */
+
+
+router.use('/root/character/:characterID', async (req, res, next) => {
+  try{
+  const reqCharacterID = parseInt(req.params.characterID, 10)
+  console.log("\n\nCharId",req.params.characterID, reqCharacterID)
+  //make sure id is a number
+  if (isNaN(reqCharacterID)) throw new Error('Character Id must be a number');
+  const rootInteraction = await Interaction.findOne({where: {
+    CharacterId: reqCharacterID,
+    isRoot: true}
+  , include: [{model:Character}]})
+  /*If rootInteraction is null, throw an error.
+    Because characters are static, there should not be case where a character has a root
+  */
+ if (!rootInteraction) throw new Error('rootInteraction must not be null')
+  res.send(rootInteraction)
+  } catch(err){
+    next(err)
+  }
+})
+
+
+router.use('/node/:interactionId', async (req,res,next) => {
  try{
   //extract id from request and check that it is a number
   const interactionId = parseInt(req.params.interactionId, 10);
@@ -11,7 +39,6 @@ router.use('/:interactionId', async (req,res,next) => {
 
   Interaction.findById(interactionId)
   .then((interaction) => {
-    console.log(interaction)
     res.send(interaction)
   })
  }catch(err) {
@@ -19,4 +46,6 @@ router.use('/:interactionId', async (req,res,next) => {
  }
 
 })
+
+
 
