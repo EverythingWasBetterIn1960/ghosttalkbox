3960232f77e24a51d183e1e38ee0dd4dd24ecaf8
Brendan Meyer <brenmeyer2245@gmail.com>
2018-10-04 06:23:05 -0500

    began rules trie

---
+++ client/utils/characterQuestionParser.js
@@ -0,0 +1,80 @@
+const ruleTrie = {
+  are: {
+    you: {
+      angry: {},
+      afraid: {},
+      at: {
+        rest: {},
+        the: {
+          pearly: {
+            gates: {}
+          }
+        }
+      },
+      happy: {},
+      sad: {},
+      holding: {
+        on: {
+          to: {
+            anything: {}
+          }
+        }
+      },
+      in: {
+        heaven: {},
+        hell: {},
+        purgatory: {},
+        pain: {},
+        denial: {}
+      }
+    }
+  },
+  can: {
+    you: {
+      see: {
+        us: {},
+        god: {},
+        my: {
+          mother: {},
+          father: {},
+          grand: {},
+          son: {},
+          daughter: {}
+        }
+      },
+      fly: {},
+      taste: {},
+      touch: {},
+      hear: {},
+      remember: {}
+    }
+  },
+  does: {
+    god: {
+      exist: {},
+      love: {
+        us: {},
+        you: {}
+      },
+      forgive: {us: {}, you: {}, me: {}, sinners: {}}
+    },
+    love: {}
+  },
+  do: {},
+  did: {},
+  has: {},
+  have: {},
+  how: {},
+  is: {
+    heaven: {
+      real: {}
+    }
+  },
+  was: {},
+  were: {},
+  who: {},
+  what: {},
+  when: {},
+  where: {},
+  why: {}
+}
